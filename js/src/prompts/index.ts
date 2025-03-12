753204cacc74103dec7b2539857da46ec8ed1841
jacoblee93 <jacoblee93@gmail.com>
2025-03-12 15:15:29 -0700

    Export prompts from their own entrypoint

---
+++ js/src/prompts/index.ts
@@ -0,0 +1,7 @@
+export { HALLUCINATION_PROMPT } from "./hallucination.js";
+export { CORRECTNESS_PROMPT } from "./correctness.js";
+export { CONCISENESS_PROMPT } from "./conciseness.js";
+export {
+  CODE_CORRECTNESS_PROMPT,
+  CODE_CORRECTNESS_PROMPT_WITH_REFERENCE_OUTPUTS,
+} from "./code_correctness.js";

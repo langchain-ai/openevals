e15c1c4fd15f1379378d8bd0bb105f511b6e91e2
jacoblee93 <jacoblee93@gmail.com>
2025-04-05 23:17:38 -0700

    Fix

--- js/vitest.config.ts
+++ js/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["dotenv/config"],
-    testTimeout: 30000
+    testTimeout: 30000,
   },
 });
